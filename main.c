b4a8e7cb79f4cb5386be2e6bcbcc3ba315ac0ad3
Linus Remahl <linusremahl@Linuss-MacBook-Pro.local>
2026-06-09 11:53:33 +0200

    Add USB serial to base code

--- main.c
+++ main.c
@@ -1,16 +1,21 @@
 #include "hal.h"
 #include "stdint.h"
+#include "usb_cdc.h"
+#include "string.h"
 
 int main(void){
     uint32_t count = 0;
+    uint8_t print_string[] = "Hello, world!\r\n";
 
     gpio_config(0, 1);
     gpio_config(1, 1);
     gpio_config(2, 1);
+    usb_cdc_init();
 
     while(1){
         for(volatile int i = 0; i < 10000000; i++);
         gpio_write(7, count++ & 7);
+        usb_cdc_transmit(print_string, strlen(print_string));
     }
     
 }
